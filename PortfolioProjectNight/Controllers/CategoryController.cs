682e0e4a9b292d889bf482c05ef05024f76c1c1e
AbdullahHamzacebi <abdullah.hmzcebi@gmail.com>
2024-10-08 15:01:12 +0300

    2.Ders Tamamlandı

---
+++ PortfolioProjectNight/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        // Sınıfın içinde yapılan yapılar field'dır.
+        public ActionResult CategoryList() // parantez varsa metod
+        {
+            // metodun içinde yapılan yapılar variable'dır.(değişkendir.)
+            return View();
+        }
+
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+    }
+}
