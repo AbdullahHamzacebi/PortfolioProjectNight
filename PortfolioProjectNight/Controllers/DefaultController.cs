5afbf717d28ac476be2868bd7e764f332366cbec
AbdullahHamzacebi <abdullah.hmzcebi@gmail.com>
2024-10-27 19:40:04 +0300

    ContactController

--- PortfolioProjectNight/Controllers/DefaultController.cs
+++ PortfolioProjectNight/Controllers/DefaultController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using PortfolioProjectNight.Models;
 
 namespace PortfolioProjectNight.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         public ActionResult Index()
         {
+
+            List<SelectListItem> values = (from x in context.Category.ToList()
+                                           select new SelectListItem
+                                           {
+                                                Text = x.CategoryName,
+                                                Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+
             return View();
+        }
+
+        [HttpPost] //bir metodun üstünde Attribute belirtilmediyse get olarak yazılır.
+        public ActionResult Index(Contact contact) 
+        {
+            contact.SendDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.IsRead = false;
+
+
+            context.Contact.Add(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public PartialViewResult PartialHead()
@@ -25,6 +48,42 @@ namespace PortfolioProjectNight.Controllers
         }
 
         public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult PartialHeader()
+        {
+            ViewBag.title = context.Profile.Select(x=>x.Title).FirstOrDefault(); 
+            ViewBag.description = context.Profile.Select(x =>x.Description).FirstOrDefault();
+            ViewBag.address = context.Profile.Select(x =>x.Address).FirstOrDefault();
+            ViewBag.email = context.Profile.Select(x =>x.Email).FirstOrDefault();
+            ViewBag.phone = context.Profile.Select(x =>x.Phone).FirstOrDefault();
+            ViewBag.github = context.Profile.Select(x =>x.Github).FirstOrDefault();
+            ViewBag.imageUrl = context.Profile.Select(x =>x.ImageUrl).FirstOrDefault();
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = context.About.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialExperience()
+        {
+            var values = context.Experience.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialSkill()
+        {
+            var values = context.Skill.Where(x =>x.Status == true).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialFooter()
         {
             return PartialView();
         }
